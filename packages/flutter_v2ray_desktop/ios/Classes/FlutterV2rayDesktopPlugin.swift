856a948ad0c2f081dd3a56e3cbce76ea8b8934a6
haha <hahah@hahah.hah>
2025-04-14 15:45:43 +0800

    解决语法问题

--- packages/flutter_v2ray_desktop/ios/Classes/FlutterV2rayDesktopPlugin.swift
+++ packages/flutter_v2ray_desktop/ios/Classes/FlutterV2rayDesktopPlugin.swift
@@ -7,7 +7,7 @@ public class FlutterV2rayDesktopPlugin: NSObject, FlutterPlugin {
         let instance = FlutterV2rayDesktopPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-
+    
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
         case "getPlatformVersion":
@@ -21,14 +21,14 @@ public class FlutterV2rayDesktopPlugin: NSObject, FlutterPlugin {
         }
     }
     // MARK: - VPN Control Methods
-
+    
     private func handleStart(call: FlutterMethodCall, result: @escaping FlutterResult) {
         guard let args = call.arguments as? [String: Any] else {
             result(FlutterError(code: "INVALID_ARGS", message: "Missing arguments", details: nil))
             return
         }
-
-
+        
+        
         let crypto = args["crypto"]
         var cryint = VPNManager.Crypto.AES256CFB
         if(crypto as! String == "aes-256-cfb"){
@@ -38,29 +38,29 @@ public class FlutterV2rayDesktopPlugin: NSObject, FlutterPlugin {
         }else if(crypto as! String == "aes-192-cfb"){
             cryint = VPNManager.Crypto.AES192CFB
         }
-
+        
         let config = VPNManager.Config.init(
             name: args["name"] as! String,
             address: args["address"] as! String,
             port: args["port"] as! Int,
             crypto: cryint,
             password: args["password"] as! String
-          )
-
+        )
+        
         let status = args["status"] as! Bool
         if(status==false){
             VPNManager.shared.connect(with: config)
         }else{
             VPNManager.shared.disconnect()
         }
-
+        
     }
-
+    
     private func handleStop(result: @escaping FlutterResult) {
         VPNManager.shared.disconnect()
-
+        
     }
-
-
-
+    
+    
+    
 }
